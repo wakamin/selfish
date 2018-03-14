b6ae26a2fbe44cf35888e7c607036dba734d8b8d
Kasmin <ginetran@gmail.com>
2018-03-14 23:02:45 +0700

    Bootstrap integration

--- functions.php
+++ functions.php
@@ -121,10 +121,12 @@ add_action( 'widgets_init', 'selfish_widgets_init' );
  */
 function selfish_scripts() {
 	wp_enqueue_style( 'selfish-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css' );
 
-	wp_enqueue_script( 'selfish-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'selfish-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'selfish-jquery', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'selfish-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'selfish-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -158,4 +160,3 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
