ca8c0963262e8cb2bf6cb25a7f3bef9528d5b370
Kasmin <ginetran@gmail.com>
2018-03-17 08:41:46 +0700

    Front page

---
+++ front-page.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The front page template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package selfish
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area container">
+		<div class="row">
+			<main id="main" class="site-main col-md-9">
+				<div class="post-list">
+
+					<?php if (have_posts()) : ?>
+
+						<?php while (have_posts()) : ?>
+							<?php the_post(); ?>
+                            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+                                <div class="row">
+                                    <div class="<?php echo (has_post_thumbnail()) ? 'col-8' : 'col-12' ?>">
+                                        <header class="entry-header">
+                                            <?php
+                                            if (is_singular()) :
+                                                the_title('<h1 class="entry-title">', '</h1>');
+                                            else :
+                                                the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" title="' . get_the_title() . '" rel="bookmark">', '</a></h2>');
+                                            endif;
+
+                                            if ('post' === get_post_type()) :
+                                                ?>
+                                            <?php endif; ?>
+                                        </header><!-- .entry-header -->
+
+                                        <div class="entry-content">
+                                            <a href="<?php esc_url(get_permalink()) ?>" title="<?php echo get_the_title() ?>" class="excerpt-link-wrapper">
+                                                <?php the_excerpt() ?>
+                                            </a>
+                                        </div><!-- .entry-content -->
+
+                                        <footer class="entry-footer">
+                                            <div class="entry-meta">
+                                                <?php
+                                                selfish_posted_on();
+                                                selfish_posted_by();
+                                                ?>
+                                            </div><!-- .entry-meta -->
+                                            <?php selfish_entry_footer(); ?>
+                                        </footer><!-- .entry-footer -->
+                                    </div>
+                                    <?php if (has_post_thumbnail()): ?>
+                                        <div class="col-4">
+                                            <a href="<?php esc_url(get_permalink()) ?>" title="<?php echo get_the_title() ?>">
+                                                <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php echo get_the_title() ?>" class="img-fluid">
+                                            </a>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+
+                            </article><!-- #post-<?php the_ID(); ?> -->
+
+						<?php endwhile; ?>
+
+						<?php the_posts_navigation(); ?>
+					<?php else : ?>
+						<?php get_template_part('template-parts/content', 'none'); ?>
+					<?php endif; ?>
+
+				</div>
+
+			</main><!-- #main -->
+
+			<?php get_sidebar(); ?>
+
+		</row><!-- .row -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
