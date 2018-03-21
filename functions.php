043df82db9a133a36160924f17e1d69c6a8410cb
Kasmin <ginetran@gmail.com>
2018-03-21 09:52:24 +0700

    WYSIWYG editor style

--- functions.php
+++ functions.php
@@ -88,22 +88,6 @@ endif;
 add_action('after_setup_theme', 'selfish_setup');
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function selfish_content_width()
-{
-    // This variable is intended to be overruled from themes.
-    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-    $GLOBALS['content_width'] = apply_filters('selfish_content_width', 640);
-}
-add_action('after_setup_theme', 'selfish_content_width', 0);
-
-/**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
@@ -143,6 +127,15 @@ function selfish_scripts()
     }
 }
 add_action('wp_enqueue_scripts', 'selfish_scripts');
+
+/**
+* Registers WYSIWYG editor stylesheet for the theme.
+*/
+function selfish_wysiwyg_editor_styles()
+{
+    add_editor_style('assets/css/wysiwyg.css');
+}
+add_action('admin_init', 'selfish_wysiwyg_editor_styles');
 
 /**
  * Implement the Custom Header feature.
